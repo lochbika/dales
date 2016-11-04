f49bdda429d7278ae9745e4166d7b392b02cad6e
lochbika <kai.lochbihler@knmi.nl>
2016-11-04 09:57:17 +0000

    reduced output in cape module

--- src/modcape.f90
+++ src/modcape.f90
@@ -33,7 +33,7 @@ private
 PUBLIC :: initcape,docape,exitcape
 save
 !NetCDF variables
-  integer,parameter :: nvar = 20
+  integer,parameter :: nvar = 4
   integer :: ncid4 = 0
   integer :: nrec = 0
   character(80) :: fname = 'cape.xxxxyxxx.xxx.nc'
@@ -85,26 +85,10 @@ contains
     fname(6:13) = cmyid
     fname(15:17) = cexpnr
     call ncinfo(tncname(1,:),'time','Time','s','time')
-    call ncinfo(ncname( 1,:),'dcape','xy crosssections of actual dcape','J/m^2','tt0t')
-    call ncinfo(ncname( 2,:),'dscape','xy crosssections of actual dscape','J/m^2','tt0t')
-    call ncinfo(ncname( 3,:),'dcin','xy crosssections of actual CIN between zcb and 1.2 zcb','J/m^2','tt0t')
-    call ncinfo(ncname( 4,:),'dscin','xy crosssections of actual CIN up to zcb','J/m^2','tt0t')
-    call ncinfo(ncname( 5,:),'dcintot','xy crosssections of total CIN up to dcape level','J/m^2','tt0t')
-    call ncinfo(ncname( 6,:),'capemax','xy crosssections of CAPEmax','J/m^2','tt0t')
-    call ncinfo(ncname( 7,:),'cinmax','xy crosssections of CIN as in CAPEmax','J/m^2','tt0t')
-    call ncinfo(ncname( 8,:),'hw2cb','xy crosssections of 1/2 W^2 at the top of the subcloud layer','m^2/s^2','tt0t')
-    call ncinfo(ncname( 9,:),'hw2max','xy crosssections of highest 1/2 W^2','m^2/s^2','tt0t')
-    call ncinfo(ncname( 10,:),'qtcb','xy crosssections of qt at cloudbase','kg/kg','tt0t')
-    call ncinfo(ncname( 11,:),'thlcb','xy crosssections of thl at cloudbase','K','tt0t')
-    call ncinfo(ncname( 12,:),'wcb','xy crosssections of w at cloudbase','m/s','tt0t')
-    call ncinfo(ncname( 13,:),'buoycb','xy crosssections buoyancy at cloudbase','K','tt0t')
-    call ncinfo(ncname( 14,:),'buoymax','xy crosssections maximum buoyancy','K','tt0t')
-    call ncinfo(ncname( 15,:),'qlcb','xy crosssections ql at cloudbase','kg/kg','tt0t')
-    call ncinfo(ncname( 16,:),'lwp','xy crosssections liquid water path','kg/m^2','tt0t')
-    call ncinfo(ncname( 17,:),'rwp','xy crosssections rain water path','kg/m^2','tt0t')
-    call ncinfo(ncname( 18,:),'twp','total water path','kg/m^2','tt0t')
-    call ncinfo(ncname( 19,:),'cldtop','xy crosssections cloud top height','m','tt0t')
-    call ncinfo(ncname( 20,:),'surfprec','surface precipitation','-','tt0t')
+    call ncinfo(ncname( 1,:),'lwp','xy crosssections liquid water path','kg/m^2','tt0t')
+    call ncinfo(ncname( 2,:),'rwp','xy crosssections rain water path','kg/m^2','tt0t')
+    call ncinfo(ncname( 3,:),'twp','total water path','kg/m^2','tt0t')
+    call ncinfo(ncname( 4,:),'surfprec','surface precipitation','-','tt0t')
     call open_nc(fname,  ncid4,nrec,n1=imax,n2=jmax)
     if (nrec==0) then
       call define_nc( ncid4, 1, tncname)
@@ -117,26 +101,19 @@ contains
 
 !>Run crosssection.
   subroutine docape
-    use modglobal, only : imax,jmax,i1,j1,k1,kmax,nsv,rlv,cp,rv,rd,rk3step,timee,rtimee,dt_lim,grav,eps1,&
-    nsv,ttab,esatltab,esatitab,zf,dzf,tup,tdn,zh,kcb
-    use modfields, only : thl0,qt0,ql0,w0,sv0,exnf,thvf,exnf,presf,rhobf
+    use modglobal, only : imax,jmax,i1,j1,k1,nsv,rk3step,timee,rtimee,dt_lim,&
+    nsv,dzf
+    use modfields, only : qt0,ql0,sv0,rhobf
     use modstat_nc, only : lnetcdf, writestat_nc
-    use modgenstat, only : qlmnlast,wthvtmnlast
     use modmicrodata, only : iqr, precep, imicro
     use modmpi
     implicit none
 
-    real, allocatable :: dcape(:,:),dscape(:,:),dcin(:,:),dscin(:,:),dcintot(:,:),capemax(:,:),&
-    cinmax(:,:),hw2cb(:,:),hw2max(:,:),qtcb(:,:),&
-    thlcb(:,:),wcb(:,:),buoycb(:,:),buoymax(:,:),qlcb(:,:),lwp(:,:),twp(:,:),rwp(:,:),&
-    cldtop(:,:),thl200400(:,:),qt200400(:,:),sprec(:,:)
-    real, allocatable :: thvfull(:,:,:),thvma(:,:,:),qlma(:,:,:),vars(:,:,:)
-    integer, allocatable :: capetop(:,:),matop(:,:)
-    logical,allocatable :: capemask(:,:,:)
+    real, allocatable :: lwp(:,:),twp(:,:),rwp(:,:),sprec(:,:)
+    real, allocatable :: vars(:,:,:)
 
     ! LOCAL VARIABLES
-    integer :: i,j,k,ktest,tlonr,thinr,niter,nitert,kdmax,kdmaxl
-    real :: Tnr,Tnr_old,ilratio,tlo,thi,esl1,esi1,qsatur,thlguess,thlguessmin,ttry,qvsl1,qvsi1
+    integer :: i,j,k
 
     if (.not. lcape) return
     if (rk3step/=3) return
@@ -147,88 +124,20 @@ contains
     tnext = tnext+idtav
     dt_lim = minval((/dt_lim,tnext-timee/))
 
-    allocate(dcape(2:i1,2:j1),dscape(2:i1,2:j1),dcin(2:i1,2:j1),dscin(2:i1,2:j1),dcintot(2:i1,2:j1))
-    allocate(capemax(2:i1,2:j1),cinmax(2:i1,2:j1),hw2cb(2:i1,2:j1))
-    allocate(thl200400(2:i1,2:j1),qt200400(2:i1,2:j1))
-    allocate(hw2max(2:i1,2:j1),qtcb(2:i1,2:j1),thlcb(2:i1,2:j1),wcb(2:i1,2:j1))
-    allocate(buoycb(2:i1,2:j1),buoymax(2:i1,2:j1),qlcb(2:i1,2:j1),&
-             lwp(2:i1,2:j1),rwp(2:i1,2:j1),cldtop(2:i1,2:j1),twp(2:i1,2:j1))
-    allocate(thvfull(2:i1,2:j1,1:k1),thvma(2:i1,2:j1,1:k1),qlma(2:i1,2:j1,1:k1),&
-             capemask(2:i1,2:j1,1:k1),capetop(2:i1,2:j1),matop(2:i1,2:j1),sprec(2:i1,2:j1))
-
-    ! DETERMINE CLOUD BASE, UNFORTUNATELY HAVE TO USE STATS HERE: END UP JUST BELOW
-    kcb=1
-    ktest=0
-    ! find robust minimum of buoyancy flux (determined at half-level!)
-    if(any(abs(wthvtmnlast)>1e-10)) then
-      do k=kmax-7,2,-1
-        if ((wthvtmnlast(k)<wthvtmnlast(k-1)).and.(wthvtmnlast(k)<wthvtmnlast(k+1)).and.&
-           (wthvtmnlast(k)<wthvtmnlast(k+2)).and.(wthvtmnlast(k)<wthvtmnlast(k+3)).and.&
-           (wthvtmnlast(k)<wthvtmnlast(k+4))) then
-          ktest = k
-        end if
-      end do
-    end if
-
-    ! Take highest half-level below which it is non-cloudy
-    if (ktest>0) then
-      do k=2,ktest
-        if(qlmnlast(k-1)<0.001) then
-        kcb=k
-        end if
-      end do
-    end if
-
-    kdmaxl=kcb
+    allocate(lwp(2:i1,2:j1),rwp(2:i1,2:j1),twp(2:i1,2:j1))
+    allocate(sprec(2:i1,2:j1))
 
     ! loops over i,j,k
     lwp=0.
     twp=0.
     rwp=0.
     sprec=0.
-    cldtop=0.
-    hw2max=0.
-    buoymax=0.
-    thl200400=0.
-    qt200400=0.
     do k=1,k1
     do j=2,j1
     do i=2,i1
-      capemask(i,j,k)=.false. ! reset
-      qlma(i,j,k)=0. ! reset
-      thvma(i,j,k)=0.
-      thvfull(i,j,k)=(thl0(i,j,k)+rlv*ql0(i,j,k)/(cp*exnf(k))) &
-                      *(1+(rv/rd-1)*qt0(i,j,k)-rv/rd*ql0(i,j,k))
       lwp(i,j)=lwp(i,j)+rhobf(k)*ql0(i,j,k)*dzf(k)
       twp(i,j)=twp(i,j)+rhobf(k)*qt0(i,j,k)*dzf(k)
-      if (ql0(i,j,k) > eps1) then
-        cldtop(i,j)=zf(k)
-      endif
-      if ((ql0(i,j,k)>eps1) .and. (thvfull(i,j,k)-thvf(k)>0)) then
-        kdmaxl=max(kdmaxl,k) ! maximum height of buoyant cloud cores
-      endif
-      if (w0(i,j,k)**2 > hw2max(i,j)) then
-        hw2max(i,j)=0.5*w0(i,j,k)*abs(w0(i,j,k))
-      endif
-      if ((thvfull(i,j,k)-thvf(k))>buoymax(i,j)) then
-        buoymax(i,j)=thvfull(i,j,k)-thvf(k)
-      endif
-      if ((k>1).and.(k<k1)) then
-      if ((zh(k+1)>200.).and.(zh(k)<400.)) then
-        thl200400(i,j)=thl200400(i,j)+thl0(i,j,k)*max(min(zh(k+1),400.)-max(zh(k),200.),0.)
-        qt200400(i,j)=qt200400(i,j)+qt0(i,j,k)*max(min(zh(k+1),400.)-max(zh(k),200.),0.)
-      endif
-      endif
     enddo
-    enddo
-    enddo
-
-    call MPI_ALLREDUCE(kdmaxl,kdmax,1,MPI_INTEGER,MPI_MAX,comm3d,mpierr)
-
-    do j=2,j1
-    do i=2,i1
-        thl200400(i,j)=thl200400(i,j)/200.
-        qt200400(i,j)=qt200400(i,j)/200.
     enddo
     enddo
 
@@ -249,212 +158,18 @@ contains
     endif
     endif
 
-    ! Cloud base level quantities and reset tops
-    do  j=2,j1
-    do  i=2,i1
-    thlcb(i,j)=thl0(i,j,kcb)
-    buoycb(i,j)=thvfull(i,j,kcb)-thvf(kcb)
-    wcb(i,j)=(w0(i,j,kcb)+w0(i,j,kcb+1))/2.
-    hw2cb(i,j)=0.5*wcb(i,j)*abs(wcb(i,j))
-    qtcb(i,j)=qt0(i,j,kcb)
-    qlcb(i,j)=ql0(i,j,kcb)
-    capetop(i,j)=1
-    matop(i,j)=0
-    enddo
-    enddo
-    !calculate moist adiabat from surface, rather than cloud base: let pressure adjust to slab mean
-
-    nitert=0
-
-    k=1
-    do j=2,j1
-    do i=2,i1
-    ! full level
-        Tnr=exnf(k)*thl200400(i,j) ! First guess for full level, use no ql from below
-        Tnr_old=0.
-        niter = 0
-          do while (abs(Tnr-Tnr_old) > 0.002) ! Find T at first level
-            niter = niter+1
-            Tnr_old=Tnr
-            ilratio = max(0.,min(1.,(Tnr-tdn)/(tup-tdn)))
-            tlonr=int((Tnr-150.)*5.)
-            thinr=tlonr+1
-            tlo=ttab(tlonr)
-            thi=ttab(thinr)
-            esl1=(thi-Tnr)*5.*esatltab(tlonr)+(Tnr-tlo)*5.*esatltab(thinr)
-            esi1=(thi-Tnr)*5.*esatitab(tlonr)+(Tnr-tlo)*5.*esatitab(thinr)
-            qsatur = ilratio*(rd/rv)*esl1/(presf(k)-(1.-rd/rv)*esl1)+(1.-ilratio)*(rd/rv)*esi1/(presf(k)-(1.-rd/rv)*esi1)
-            thlguess = Tnr/exnf(k)-(rlv/(cp*exnf(k)))*max(qt200400(i,j)-qsatur,0.)
-
-            ttry=Tnr-0.002
-            ilratio = max(0.,min(1.,(ttry-tdn)/(tup-tdn)))
-            tlonr=int((Tnr-150.)*5.)
-            thinr=tlonr+1
-            tlo=ttab(tlonr)
-            thi=ttab(thinr)
-            esl1=(thi-ttry)*5.*esatltab(tlonr)+(ttry-tlo)*5.*esatltab(thinr)
-            esi1=(thi-ttry)*5.*esatitab(tlonr)+(ttry-tlo)*5.*esatitab(thinr)
-            qsatur = ilratio*(rd/rv)*esl1/(presf(k)-(1.-rd/rv)*esl1)+(1.-ilratio)*(rd/rv)*esi1/(presf(k)-(1.-rd/rv)*esi1)
-            thlguessmin = ttry/exnf(k)-(rlv/(cp*exnf(k)))*max(qt200400(i,j)-qsatur,0.)
-
-            Tnr = Tnr - (thlguess-thl200400(i,j))/((thlguess-thlguessmin)*500.)
-          enddo
-        nitert =max(nitert,niter)
-        niter = 0
-        ilratio = max(0.,min(1.,(Tnr-tdn)/(tup-tdn)))
-        tlonr=int((Tnr-150.)*5.)
-        thinr=tlonr+1
-        tlo=ttab(tlonr)
-        thi=ttab(thinr)
-        esl1=(thi-Tnr)*5.*esatltab(tlonr)+(Tnr-tlo)*5.*esatltab(thinr)
-        esi1=(thi-Tnr)*5.*esatitab(tlonr)+(Tnr-tlo)*5.*esatitab(thinr)
-        qvsl1=rd/rv*esl1/(presf(k)-(1.-rd/rv)*esl1)
-        qvsi1=rd/rv*esi1/(presf(k)-(1.-rd/rv)*esi1)
-        qsatur = ilratio*qvsl1+(1.-ilratio)*qvsi1
-        qlma(i,j,k) = max(qt200400(i,j)-qsatur,0.)
-        thvma(i,j,k)=(thl200400(i,j)+(rlv*qlma(i,j,k))/(cp*exnf(k)))&
-                     *(1+(rv/rd-1)*qt200400(i,j)-rv/rd*qlma(i,j,k)) ! calculate thv
-    enddo
-    enddo
-    do k=2,k1
-    do j=2,j1
-    do i=2,i1
-    ! full level
-      if(matop(i,j)==0) then
-        Tnr=exnf(k)*thl200400(i,j)+(rlv/cp)*qlma(i,j,k-1) ! Guess for full level
-        Tnr_old=0.
-        niter=0
-          do while (abs(Tnr-Tnr_old) > 0.002) ! Find T at level
-            niter = niter+1
-            Tnr_old=Tnr
-            ilratio = max(0.,min(1.,(Tnr-tdn)/(tup-tdn)))
-            tlonr=int((Tnr-150.)*5.)
-            thinr=tlonr+1
-            tlo=ttab(tlonr)
-            thi=ttab(thinr)
-            esl1=(thi-Tnr)*5.*esatltab(tlonr)+(Tnr-tlo)*5.*esatltab(thinr)
-            esi1=(thi-Tnr)*5.*esatitab(tlonr)+(Tnr-tlo)*5.*esatitab(thinr)
-            qsatur = ilratio*(rd/rv)*esl1/(presf(k)-(1.-rd/rv)*esl1)+(1.-ilratio)*(rd/rv)*esi1/(presf(k)-(1.-rd/rv)*esi1)
-            thlguess = Tnr/exnf(k)-(rlv/(cp*exnf(k)))*max(qt200400(i,j)-qsatur,0.)
-
-            ttry=Tnr-0.002
-            ilratio = max(0.,min(1.,(ttry-tdn)/(tup-tdn)))
-            tlonr=int((Tnr-150.)*5.)
-            thinr=tlonr+1
-            tlo=ttab(tlonr)
-            thi=ttab(thinr)
-            esl1=(thi-ttry)*5.*esatltab(tlonr)+(ttry-tlo)*5.*esatltab(thinr)
-            esi1=(thi-ttry)*5.*esatitab(tlonr)+(ttry-tlo)*5.*esatitab(thinr)
-            qsatur = ilratio*(rd/rv)*esl1/(presf(k)-(1.-rd/rv)*esl1)+(1.-ilratio)*(rd/rv)*esi1/(presf(k)-(1.-rd/rv)*esi1)
-            thlguessmin = ttry/exnf(k)-(rlv/(cp*exnf(k)))*max(qt200400(i,j)-qsatur,0.)
-
-            Tnr = Tnr - (thlguess-thl200400(i,j))/((thlguess-thlguessmin)*500.)
-          enddo
-        nitert =max(nitert,niter)
-        niter = 0
-        ilratio = max(0.,min(1.,(Tnr-tdn)/(tup-tdn)))
-        tlonr=int((Tnr-150.)*5.)
-        thinr=tlonr+1
-        tlo=ttab(tlonr)
-        thi=ttab(thinr)
-        esl1=(thi-Tnr)*5.*esatltab(tlonr)+(Tnr-tlo)*5.*esatltab(thinr)
-        esi1=(thi-Tnr)*5.*esatitab(tlonr)+(Tnr-tlo)*5.*esatitab(thinr)
-        qvsl1=rd/rv*esl1/(presf(k)-(1.-rd/rv)*esl1)
-        qvsi1=rd/rv*esi1/(presf(k)-(1.-rd/rv)*esi1)
-        qsatur = ilratio*qvsl1+(1.-ilratio)*qvsi1
-        qlma(i,j,k) = max(qt200400(i,j)-qsatur,0.)
-        thvma(i,j,k)=(thl200400(i,j)+(rlv*qlma(i,j,k))/(cp*exnf(k)))&
-                     *(1+(rv/rd-1)*qt200400(i,j)-rv/rd*qlma(i,j,k)) ! calculate thv
-        if(thvma(i,j,k)<thvf(k)-10) then
-          matop(i,j)=k
-        elseif(k==k1) then
-          matop(i,j)=k
-        endif
-      endif
-    enddo
-    enddo
-    enddo
-
-    ! calculate top of moist adiabat for dcape calculations
-    do k=1,k1
-    do j=2,j1
-    do i=2,i1
-      if(k<matop(i,j)) then
-        if(thvma(i,j,k)>thvf(k)) then
-        capetop(i,j)=k
-        capemask(i,j,k)=.true.
-        endif
-      endif
-    enddo
-    enddo
-    enddo
-
-    ! dcape and CIN of Moist Adiabat
-    ! no nice interpolation yet
-    do j=2,j1
-    do i=2,i1
-    capemax(i,j)=0.
-    cinmax(i,j)=0.
-    dcape(i,j)=0.
-    dcin(i,j)=0.
-    dscin(i,j)=0.
-    dcintot(i,j)=0.
-    dscape(i,j)=0.
-    enddo
-    enddo
-
-    do k=1,k1
-    do j=2,j1
-    do i=2,i1
-      if(k<capetop(i,j)) then
-        cinmax(i,j)=cinmax(i,j)+max(grav*dzf(k)*(thvf(k)-thvma(i,j,k))/thvf(k),0.)
-        capemax(i,j)=capemax(i,j)+max(grav*dzf(k)*(thvma(i,j,k)-thvf(k))/thvf(k),0.)
-      endif
-     if(k<kdmax) then
-        if(k<kcb) then
-            dscape(i,j)=dscape(i,j)+max(grav*dzf(k)*(thvfull(i,j,k)-thvf(k))/thvf(k),0.)
-            dscin(i,j)=dscin(i,j)+max(grav*dzf(k)*(thvf(k)-thvfull(i,j,k))/thvf(k),0.)
-        else
-            dcape(i,j)=dcape(i,j)+max(grav*dzf(k)*(thvfull(i,j,k)-thvf(k))/thvf(k),0.)
-        endif
-            dcintot(i,j)=dcintot(i,j)+max(grav*dzf(k)*(thvf(k)-thvfull(i,j,k))/thvf(k),0.)
-        if((zf(k)<1.2*zf(kcb)).and.(zf(k)>zf(kcb))) then
-            dcin(i,j)=dcin(i,j)+max(grav*dzf(k)*(thvf(k)-thvfull(i,j,k))/thvf(k),0.)
-        endif
-      endif
-    enddo
-    enddo
-    enddo
-
     if (lnetcdf) then
       allocate(vars(1:imax,1:jmax,nvar))
-      vars(:,:,1) = dcape(2:i1,2:j1)
-      vars(:,:,2) = dscape(2:i1,2:j1)
-      vars(:,:,3) = dcin(2:i1,2:j1)
-      vars(:,:,4) = dscin(2:i1,2:j1)
-      vars(:,:,5) = dcintot(2:i1,2:j1)
-      vars(:,:,6) = capemax(2:i1,2:j1)
-      vars(:,:,7) = cinmax(2:i1,2:j1)
-      vars(:,:,8) = hw2cb(2:i1,2:j1)
-      vars(:,:,9) = hw2max(2:i1,2:j1)
-      vars(:,:,10) = qtcb(2:i1,2:j1)
-      vars(:,:,11)= thlcb(2:i1,2:j1)
-      vars(:,:,12) = wcb(2:i1,2:j1)
-      vars(:,:,13) = buoycb(2:i1,2:j1)
-      vars(:,:,14) = buoymax(2:i1,2:j1)
-      vars(:,:,15) = qlcb(2:i1,2:j1)
-      vars(:,:,16) = lwp(2:i1,2:j1)
-      vars(:,:,17) = rwp(2:i1,2:j1)
-      vars(:,:,18) = twp(2:i1,2:j1)
-      vars(:,:,19) = cldtop(2:i1,2:j1)
-      vars(:,:,20)= sprec(2:i1,2:j1)
+      vars(:,:,1) = lwp(2:i1,2:j1)
+      vars(:,:,2) = rwp(2:i1,2:j1)
+      vars(:,:,3) = twp(2:i1,2:j1)
+      vars(:,:,4)= sprec(2:i1,2:j1)
       call writestat_nc(ncid4,1,tncname,(/rtimee/),nrec,.true.)
       call writestat_nc(ncid4,nvar,ncname(1:nvar,:),vars,nrec,imax,jmax)
       deallocate(vars)
     end if
 
-    deallocate(dcape,dscape,dcin,dscin,dcintot,capemax,cinmax,hw2cb,hw2max,qtcb,thlcb,wcb,&
-    buoycb,buoymax,qlcb,lwp,twp,rwp,cldtop,thvfull,thvma,qlma,capemask,capetop,matop,thl200400,qt200400,sprec)
+    deallocate(lwp,twp,rwp,sprec)
 
   end subroutine docape
 
